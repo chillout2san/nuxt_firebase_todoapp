a8d9973cebad538fe2aa2a01af295169b2225f37
2san <chillout2san@gamil.com>
2021-04-28 19:59:35 +0900

    20210428 メールサインアップ実装中

---
+++ components/signin_signup/Signin.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="columns">
+    <div class="column">
+
+      <div class="field">
+        <label class="label">ユーザー名</label>
+        <div class="control">
+          <input
+            class="input is-success"
+            type="text"
+            placeholder="ユーザー名を入力してください"
+          />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">メールアドレス</label>
+        <div class="control">
+          <input
+            class="input is-success"
+            type="email"
+            placeholder="メールアドレスを入力してください"
+          />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">パスワード</label>
+        <div class="control">
+          <input
+            class="input is-success"
+            type="email"
+            placeholder="パスワードを入力してください"
+          />
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancel</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+        こんにちは
+    </div>
+
+  </div>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -17,13 +17,19 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['bulma'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      '~/components',
+      '~/components/header_footer',
+      '~/components/signin_signup',
+    ]
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
