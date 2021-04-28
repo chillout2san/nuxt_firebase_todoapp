fbf333d480e0121aa7fdf0d8c6c5266443d1480e
2san <chillout2san@gamil.com>
2021-04-28 18:01:54 +0900

    20210428 setting about eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,11 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'prettier'
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+  },
 }
